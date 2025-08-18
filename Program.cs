633c08b87b9f66131e981fe98a02b66ba42cc9de
Godwin Ofwono <godwinofwono933@gmail.com>
2025-08-18 17:59:42 +0300

    Initial Commit of repo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Am Now Understanding C#");
+string firstFriend = "Ojara";
+string secondFriend = "Kevin";
+Console.WriteLine("My friends are " +firstFriend + " and  " + secondFriend);
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+//Trimming Strings
+string soda = "       Fanta       ";
+soda = soda.Trim();
+Console.WriteLine(soda);
+Console.WriteLine(soda.Replace("Fanta ", "Mirinda"));
+Console.WriteLine(soda);
