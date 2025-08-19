32f4627469fa4d7b1644302b0511af40d29de034
Godwin Ofwono <godwinofwono933@gmail.com>
2025-08-19 12:52:31 +0300

    Worked on Dealing with Numbers in C

--- Program.cs
+++ Program.cs
@@ -3,12 +3,15 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Am Now Understanding C#");
 string firstFriend = "Ojara";
 string secondFriend = "Kevin";
-Console.WriteLine("My friends are " +firstFriend + " and  " + secondFriend);
-Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+int age = 22;
+int ages = 20;
+float w1 = 60.5f;
+float w2 = 65.2f;
 
+Console.WriteLine($"My friends are {firstFriend.ToUpper()} and {secondFriend}");
+Console.WriteLine($"{firstFriend.ToUpper()} is {age} years old and {secondFriend.ToLower()} is {ages} years old");
+Console.WriteLine($"{firstFriend} weighs {w1} kgs and {secondFriend} weighs {w2} kgs ");
 //Trimming Strings
 string soda = "       Fanta       ";
-soda = soda.Trim();
-Console.WriteLine(soda);
-Console.WriteLine(soda.Replace("Fanta ", "Mirinda"));
-Console.WriteLine(soda);
+Console.WriteLine(soda.Trim().Replace("Fanta","Mirinda").ToLower());
+
